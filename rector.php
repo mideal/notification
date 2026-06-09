5c3620482b93c53793203d33f063c1934a964b24
Denis Mitrofanov <midealf@gmail.com>
2026-06-10 01:34:13 +0300

    rector

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        // __DIR__ . '/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([
+        __DIR__.'/bootstrap/cache',
+    ])
+    // register single rule
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
+    ])
+    // here we can define, what prepared sets of rules will be applied
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+    );
